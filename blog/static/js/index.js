d73a4d260aabd1dd1544c1f1017bbfb336ed7c12
jncheng <chengjn@xueshandai.com>
2017-02-21 18:56:44 +0800

    修改文章详情页样式

--- blog/static/js/index.js
+++ blog/static/js/index.js
@@ -1,10 +1,9 @@
-var oMenu = $('a');
-var old_color = oMenu.css('color');
-
-oMenu.mouseenter(function() {
+//修改当鼠标经过a标签时改变颜色
+var oA = $('a');
+var old_color = oA.css('color');
+oA.mouseenter(function() {
     $(this).css('color','red').css('text-decoration','none');
 });
-
-oMenu.mouseleave(function() {
+oA.mouseleave(function() {
     $(this).css('color',old_color);
 });
