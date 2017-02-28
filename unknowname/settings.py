7cf582e596e1bf261ef477106e3bbd3cb6bb1eff
jncheng <jncheng@xueshandai.com>
2017-02-28 10:36:09 +0800

    静态资源规范化，增加Docker支持

--- unknowname/settings.py
+++ unknowname/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'a=rl%wo(*d%rn6@=kq%(^kdo*j69kpru&7w51mog_ly_g8#0im'
 if socket.gethostname() == 'ubuntu12':
     DEBUG = True
 else:
+    #DEBUG = False
     DEBUG = True
 
 ALLOWED_HOSTS = ['*']
