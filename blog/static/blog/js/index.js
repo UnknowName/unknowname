345a94c1d35e46542357c472f7130c9e2813034d
jncheng <jncheng@xueshandai.com>
2017-03-08 15:24:51 +0800

    修改JS调整样式

--- blog/static/blog/js/index.js
+++ blog/static/blog/js/index.js
@@ -7,3 +7,9 @@ oA.mouseenter(function() {
 oA.mouseleave(function() {
     $(this).css('color',old_color);
 });
+//修改首页菜单与内容边框不对齐
+var oMenu = $('#menu ul');
+var oMain = $('#main-right');
+if(oUl.width() > oMain.width()) {
+    oUl.css('padding','0px 205px');
+};
